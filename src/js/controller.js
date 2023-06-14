4b0b208c794caa36355b13710318ac2fd17264a9
Tonyfed1 <obiora.onah3@gmail.com>
2023-06-14 12:32:48 +0100

    New Features

--- src/js/controller.js
+++ src/js/controller.js
@@ -104,5 +104,6 @@ const init = function () {
   paginationView.addHandlerClick(controlPagination)
   addRecipeView.addHandlerUpload(controlAddRecipe)
   console.log('Welcome')
+  alert('HACKED')
 }
 init()
