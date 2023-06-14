2cf80cee20e0b5750608188fe0a526669cb3fe81
Tonyfed1 <obiora.onah3@gmail.com>
2023-06-14 15:19:08 +0100

    updated1

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,7 +3,7 @@ import recipeView from './views/recipeView.js'
 import searchView from './views/searchView.js'
 import resultsView from './views/resultsView.js'
 import paginationView from './views/paginationView.js'
-import bookmarksView from './views/BookmarksView.js'
+import bookmarksView from './views/bookmarksView.js'
 import addRecipeView from './views/addRecipeView.js'
 
 import 'core-js/stable'
